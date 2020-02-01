aaa8ea0f65334426a7301af732d9c52bfb355474
League Student <league@iMac-9.attlocal.net>
2020-02-01 11:26:43 -0800

    Update _02_FindTheHiddenButton.java

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -40,14 +40,14 @@ public class _02_FindTheHiddenButton implements ActionListener {
 		b = Integer.parseInt(number);
 
 		// 4. Initialize the array of JButtons to be the size of the int created in step
-		
+
 		JButton[] j = new JButton[b];
 
 		// 5. Make a for loop to iterate through the JButton array
 
 		for (int i = 0; i < j.length; i++) {
 			// 6. initialize each JButton in the array
-			j[i] = new JButton();
+			j[i]= new JButton();
 
 			// 7. add the ActionListener to each JButton
 			j[i].addActionListener(this);
@@ -63,7 +63,7 @@ public class _02_FindTheHiddenButton implements ActionListener {
 		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
 		// 11. set the JFrame to visible.
-		window.setVisible(true);
+     		window.setVisible(true);
 
 		// 12. Give the user the instructions for the game.
 		JOptionPane.showMessageDialog(null, "Find the hidden button.");
@@ -79,16 +79,16 @@ public class _02_FindTheHiddenButton implements ActionListener {
 
 		// 15. Use Thread.sleep(1000); to pause the program.
 		// Surround it with a try/catch - use Eclipse helper for this
-		try {
-			Thread.sleep(500);
+	   	try {
+			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+	   	
 		// 16. Set the text of the JButton located at hiddenButton to be blank.
 		j[hiddenButton].setText("  ");
-		
+
 	}
 
 	@Override
@@ -96,13 +96,13 @@ public class _02_FindTheHiddenButton implements ActionListener {
 		JButton buttonClicked = (JButton) e.getSource();
 
 		// 17. if the hiddenButton is clicked, tell the user that they win.
-		if(e.getSource()==j[hiddenButton]) {
+		if (e.getSource() == j[hiddenButton]) {
 			JOptionPane.showMessageDialog(null, " You win. ");
 		}
 
 		// 18. else tell them to try again
 		else {
-			JOptionPane.showMessageDialog(null,  "Try again.");
+			JOptionPane.showMessageDialog(null, "Try again.");
 		}
 	}
 }
